72852d4b3cf66cf1ec23b0c47af2c8ec0fefb45b
sandesh ghoti <sandesh.ghot@gmail.com>
2024-01-10 18:50:02 +0000

    added indexing

--- src/models/like.js
+++ src/models/like.js
@@ -1,23 +1,31 @@
 const mongoose = require("mongoose");
-const {ModelName}=require('../utils/common')
-const {Tweet, Comment}=ModelName
-const likeSchema = new mongoose.Schema({
-    userId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User',
-        required:true
+const { ModelName } = require("../utils/common");
+const { Tweet, Comment } = ModelName;
+const likeSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    onModel:{
-        type:String,
-        enum:[Tweet,Comment],
-        required:true
+    onModel: {
+      type: String,
+      enum: [Tweet, Comment],
+      required: true,
     },
-    likedOn:{
-        type:mongoose.Schema.Types.ObjectId,
-        refPath:'onModel',
-        required:true,
-    }
-},{timestamps: true});
+    likedOn: {
+      type: mongoose.Schema.Types.ObjectId,
+      refPath: "onModel",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
+likeSchema.index({
+  userId: 1,
+  onModel: 1,
+  likedOn: -1,
+});
 const like = mongoose.model("Like", likeSchema);
 module.exports = like;
